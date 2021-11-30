6f770fa5a1e401453c3dee216e6ff0ac2c4cb97b
mythiliram56 <priya1151814@gmail.com>
2021-11-30 12:30:09 +0530

    initial commit

--- app/src/androidTest/java/myproject/room/roomdatabasesample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/myproject/room/roomdatabasesample/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.roomdatabasesample
+package myproject.room.roomdatabasesample
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
